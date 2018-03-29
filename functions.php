3084c5407432bbf48d695d4697eec625011a87cf
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-03-29 23:19:04 +0200

    Created custom menues

--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
 
-  function apollo1_script_enqueue() {
+function apollo1_script_enqueue() {
 
-    wp_enqueue_style('customstyle', get_template_directory_uri() .' /css/apollo1.css', array(), '1.0.0', 'all');
-    wp_enqueue_script('customjs', get_template_directory_uri() . '/js/apollo1.js', array(), '1.0.0', true);
+  wp_enqueue_style('customstyle', get_template_directory_uri() .' /css/apollo1.css', array(), '1.0.0', 'all');
+  wp_enqueue_script('customjs', get_template_directory_uri() . '/js/apollo1.js', array(), '1.0.0', true);
 
 }
 
 add_action('wp_enqueue_scripts', 'apollo1_script_enqueue');
+
+
+function apollo1_theme_setup() {
+
+  add_theme_support('menus');
+  register_nav_menu('primary', 'Primary Header Navigation');
+  register_nav_menu('secondary', 'Footer Navigation');
+
+}
+
+add_action('init', 'apollo1_theme_setup');
--- header.php
+++ header.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Chung Win's Apollo 1</title>
-    <?php wp_head(); ?> 
+    <?php wp_head(); ?>
   </head>
 
   <body>
+
+      <?php wp_nav_menu(array('theme_location'=>'primary')) ?>
