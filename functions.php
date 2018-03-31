62d54a33a8e958904ecc6e5976a919bafd81a687
Chungwin <37634612+Chungwin@users.noreply.github.com>
2018-03-31 13:08:21 +0200

    Install Bootstrap

--- functions.php
+++ functions.php
@@ -1,14 +1,25 @@
 <?php
-
+/*
+  ==================================
+  Include Scripts
+  ==================================
+*/
 function apollo1_script_enqueue() {
-
+//css
+  wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0', 'all');
   wp_enqueue_style('customstyle', get_template_directory_uri() .' /css/apollo1.css', array(), '1.0.0', 'all');
+//js
+  wp_enqueue_style('jquery');
+  wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '4.0.0', true);
   wp_enqueue_script('customjs', get_template_directory_uri() . '/js/apollo1.js', array(), '1.0.0', true);
-
 }
 
 add_action('wp_enqueue_scripts', 'apollo1_script_enqueue');
-
+/*
+  ==================================
+  Activate Menus
+  ==================================
+*/
 
 function apollo1_theme_setup() {
 
@@ -20,6 +31,11 @@ function apollo1_theme_setup() {
 
 add_action('init', 'apollo1_theme_setup');
 
+/*
+  ==================================
+  Theme support functions
+  ==================================
+*/
 add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
